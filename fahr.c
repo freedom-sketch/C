797945f13730816a6fd464bf4bae80b4e58eec7a
orex2105 <pokomisa55@gmail.com>
2026-02-16 00:48:40 +0300

    fahr.c

--- fahr.c
+++ fahr.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main() {
-    int fahr, celsius;
+    float fahr, celsius;
     int lower, upper, step;
 
     lower = 0; /* нижняя граница таблицы температур */
@@ -13,8 +13,8 @@ int main() {
 
     fahr = lower;
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0 / 9.0) * (fahr-32.0);
+        printf("%3d %6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 
