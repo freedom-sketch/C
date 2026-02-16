7e5c08bfa64b2b28e3a593a7c4ea5e2fa467d103
orex2105 <pokomisa55@gmail.com>
2026-02-16 15:09:17 +0300

    fahr2.c

--- fahr2.c
+++ fahr2.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 
-void temperature_table() {
-    printf("%3s\t%6s\n", " ℉", "°C ");
-    for (int fahr = 300; fahr >= 0; fahr -= 20)
-        printf("%3d%6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+#define LOWER  0
+#define UPPER  300
+#define STEP   20
+
+static void print_temperature_table(int start, int end, int step) {
+    printf("%3s\t%6s\n", "°F", "°C");
+
+    int fahr = start;
+    while (1) {
+        printf("%3d%7.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+        if (fahr == end) break;
+        fahr += step;
+    }
 }
 
-void reverse_temperature_table() {
-    printf("%3s\t%6s\n", " ℉", "°C ");
-    for (int fahr = 0; fahr <= 300; fahr += 20)
-        printf("%3d%6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
-}
-
-int main() {
-    temperature_table();
-    reverse_temperature_table();
+int main(void) {
+    print_temperature_table(UPPER, LOWER, -STEP);
+    printf("\n");
+    print_temperature_table(LOWER, UPPER,  STEP);
 
     return 0;
 }
