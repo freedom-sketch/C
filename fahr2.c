5693a6d6a558549322e286d3e4aa509f17860bc1
orex2105 <pokomisa55@gmail.com>
2026-02-16 14:43:33 +0300

    fahr2.c с выполненными заданиями

--- fahr2.c
+++ fahr2.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-int main() {
+void temperature_table() {
     printf("%3s\t%6s\n", " ℉", "°C ");
-    for (int fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (int fahr = 300; fahr >= 0; fahr -= 20)
         printf("%3d%6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+}
+
+void reverse_temperature_table() {
+    printf("%3s\t%6s\n", " ℉", "°C ");
+    for (int fahr = 0; fahr <= 300; fahr += 20)
+        printf("%3d%6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+}
+
+int main() {
+    temperature_table();
+    reverse_temperature_table();
 
     return 0;
 }
