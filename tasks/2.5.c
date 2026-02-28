eb291eca7be03b5bc8d0a292ea711cd03ad815c2
freedom <pokomisa55@gmail.com>
2026-02-28 21:12:26 +0300

    2.5

--- tasks/2.5.c
+++ tasks/2.5.c
@@ -2,9 +2,22 @@
 
 int any(char s1[], char s2[])
 {
-    signed int result = -1;
-    
     for (int i = 0; s1[i] != '\0'; ++i) {
-        
+        for (int j = 0; s2[j] != '\0'; ++j) {
+            if (s1[i] == s2[j])
+                return i;
+        }
     }
+
+    return -1;
+}
+
+int main(void)
+{
+    char s1[] = "abcdefg";
+    char s2[] = "gfe";
+
+    printf("%d", any(s1, s2));
+
+    return 0;
 }
